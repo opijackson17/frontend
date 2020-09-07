439e1100eb076b28ed811e5783de0001b9a14ded
opijackson17 <opijackson17@gmail.com>
2020-09-07 14:17:08 +0300

    Fixed many requests to the server

--- src/FetchData.js
+++ src/FetchData.js
@@ -3,25 +3,34 @@ import {Row, Col} from 'react-bootstrap';
 
 function FetchData(){
 
-  const [errors, setErrors] = useState(false);
-  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
 
     useEffect(() => {
-       function fetchData(){
          fetch('http://localhost:80/api/universities').then(res => res.json())
-         .then(result => setData(result))
-         .catch((error) => setErrors(error));
-    
-            }
-            fetchData();
-        });
-
-        const listitems = data.map((item) =>
-          <Row className="mt-2 mb-2"><Col><span className = "pr-2"><input type="radio" value={ item.name} className="form-group" name="university" /></span>{ item.name}
-           </Col></Row>        
-        );
-
-        return  errors ? JSON.stringify(errors):listitems;      
+         .then(
+          (result) => {
+           setIsLoaded(true);
+           setItems(result);
+         },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+        }
+      )
+    }, []);
+    if (error) {
+      return <Row className="mt-2 mb-2"><Col><span className = "pr-2">Error: {error.message}</span></Col></Row>;
+    } else if (!isLoaded) {
+      return <Row className="mt-2 mb-2"><Col><span className = "pr-2">Loading...</span></Col></Row>;
+    } else {
+      return (<div>{
+        items.map((item) => <Row className="mt-2 mb-2"><Col><span className = "pr-2"><input type="radio" value={ item.id} className="form-group" name="university" /></span>{ item.name}
+       </Col></Row> )
+       }</div>)
+    }
 }
 
 export default FetchData;
